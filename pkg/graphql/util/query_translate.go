35a80f92eb02e40daff5924085a489ce45cb2869
wenyao.wu <835802084@qq.com>
2020-12-04 23:14:19 +0800

    update

--- pkg/graphql/util/query_translate.go
+++ pkg/graphql/util/query_translate.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+	"errors"
+	"fmt"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"reflect"
 )
 
@@ -28,57 +31,118 @@ func NewQueryTranslator(tx *gorm.DB, model interface{}) *QueryTranslator {
 }
 
 // 排序
-func (t *QueryTranslator) OrderBy(orderBy interface{}) (r *QueryTranslator) {
-	r = t
-	r.orderBy = orderBy
-	return r
+func (t *QueryTranslator) OrderBy(orderBy interface{}) (re *QueryTranslator) {
+	re = t
+	if orderBy == nil {
+		return re
+	}
+	re.orderBy = orderBy
+
+	orderByValue := getValue(orderBy)
+	orderByKind := orderByValue.Kind()
+	switch orderByKind {
+	case reflect.Slice:
+		sliceLength := orderByValue.Len()
+		for i := 0; i < sliceLength; i++ {
+			orderByItem := getValue(orderByValue.Index(i))
+			re.tx = re.tx.Order(buildOrderBy(orderByItem))
+		}
+	default:
+		panic(errors.New("unSupport type"))
+	}
+	return re
 }
 
 // 设置offset
-func (t *QueryTranslator) Offset(offset *int) (r *QueryTranslator) {
-	r = t
-	r.offset = offset
-	return r
+func (t *QueryTranslator) Offset(offset *int) (re *QueryTranslator) {
+	re = t
+	if offset == nil {
+		return re
+	}
+	re.offset = offset
+
+	re.tx = re.tx.Offset(*offset)
+	return re
 }
 
 // 设置limit
-func (t *QueryTranslator) Limit(limit *int) (r *QueryTranslator) {
-	r = t
-	r.limit = limit
-	// 执行
-
-	return r
+func (t *QueryTranslator) Limit(limit *int) (re *QueryTranslator) {
+	re = t
+	if limit == nil {
+		return re
+	}
+	re.limit = limit
+	re.tx = re.tx.Limit(*limit)
+	return re
 }
 
 // 设置distinctOn
-func (t *QueryTranslator) DistinctOn(distinctOn interface{}) (r *QueryTranslator) {
-	r = t
-	r.distinctOn = distinctOn
-	// 翻译district on
-	ty := reflect.TypeOf(distinctOn)
+func (t *QueryTranslator) DistinctOn(distinctOn interface{}) (re *QueryTranslator) {
+	re = t
+	if distinctOn == nil {
+		return re
+	}
+	re.distinctOn = distinctOn
 
-	k := ty.Kind()
-	switch k {
+	distinctOnValue := getValue(distinctOn)
+	distinctOnKind := distinctOnValue.Kind()
+	switch distinctOnKind {
 	case reflect.Slice:
-		
-
+		sliceLength := distinctOnValue.Len()
+		sArray := make([]string, sliceLength)
+		for i := 0; i < sliceLength; i++ {
+			sArray[i] = distinctOnValue.Index(i).String()
+		}
+		re.tx = re.tx.Distinct(sArray)
+	default:
+		panic(errors.New("unSupport type"))
 	}
-	if sArray, ok := distinctOn.([]interface{}); ok {
-		t.tx = t.tx.Distinct(sArray)
-	}
 
-	return r
+	return re
 }
 
 // 设置where
-func (t *QueryTranslator) Where(where interface{}) (r *QueryTranslator) {
-	r = t
-	r.where = where
-	return r
+func (t *QueryTranslator) Where(where interface{}) (re *QueryTranslator) {
+	re = t
+	if where == nil {
+		return re
+	}
+	re.where = where
+
+	return re
 }
 
 // 执行翻译graphql到sql到操作
 func (t *QueryTranslator) DoTranslate() *gorm.DB {
 
 	return t.tx
+}
+
+// 获取reflect.value
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
+}
+
+// 反射构建order
+func buildOrderBy(value reflect.Value) clause.OrderByColumn {
+	// 获取orderByItem第一个非nil的属性和这个属性的tag
+	// 即为 order的方向和属性
+	valueKind := value.Kind()
+	switch valueKind {
+	case reflect.Struct:
+		for i := 0; i < value.NumField(); i++ {
+			fieldValue := value.Field(i)
+			fmt.Println(fieldValue)
+		}
+	default:
+		panic(errors.New("unSupport type"))
+	}
+
+	return clause.OrderByColumn{}
 }
