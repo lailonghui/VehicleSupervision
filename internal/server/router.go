88c5a8fb3781c653ddc5a48b8ff6db127d0e99a4
Your Name <you@example.com>
2020-12-08 15:01:08 +0800

    change schema,add endpoint

--- internal/server/router.go
+++ internal/server/router.go
@@ -2,9 +2,15 @@ package server
 
 import (
 	"VehicleSupervision/config"
-	enterprise_mutation "VehicleSupervision/internal/modules/admin/enterprise/mutation"
-	enterprise_query "VehicleSupervision/internal/modules/admin/enterprise/query"
-	systemuser_query "VehicleSupervision/internal/modules/admin/systemuser/query"
+	adasMutation "VehicleSupervision/internal/modules/adas/mutation"
+	adasQuery "VehicleSupervision/internal/modules/adas/query"
+	enterpriseMutation "VehicleSupervision/internal/modules/admin/enterprise/mutation"
+	enterpriseQuery "VehicleSupervision/internal/modules/admin/enterprise/query"
+	systemUserQuery "VehicleSupervision/internal/modules/admin/systemuser/query"
+	areaMutation "VehicleSupervision/internal/modules/area/mutation"
+	areaQuery "VehicleSupervision/internal/modules/area/query"
+	trainingMutation "VehicleSupervision/internal/modules/training/mutation"
+	trainingQuery "VehicleSupervision/internal/modules/training/query"
 	"VehicleSupervision/pkg/logger"
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -43,9 +49,21 @@ func Setup() {
 	//router.Any("/vehicle", vehicle.GinEndpoint())
 	//驾驶员模块端点
 	//router.Any("/driver", driver.GinEndpoint())
-	router.Any("/enterprise/query", enterprise_query.GinEndpoint())
-	router.Any("/enterprise/mutation", enterprise_mutation.GinEndpoint())
-	router.Any("/system_user/query", systemuser_query.GinEndpoint())
+	router.Any("/enterprise/query", enterpriseQuery.GinEndpoint())
+	router.Any("/enterprise/mutation", enterpriseMutation.GinEndpoint())
+	router.Any("/system_user/query", systemUserQuery.GinEndpoint())
+
+	//adas模块端点
+	router.Any("/adas/query", adasQuery.GinEndpoint())
+	router.Any("/adas/mutation", adasMutation.GinEndpoint())
+
+	//地区模块端点
+	router.Any("/area/query", areaQuery.GinEndpoint())
+	router.Any("/area/mutation", areaMutation.GinEndpoint())
+
+	//教育模块端点
+	router.Any("/training/query", trainingQuery.GinEndpoint())
+	router.Any("/training/mutation", trainingMutation.GinEndpoint())
 
 	addr := fmt.Sprintf("%s:%d", host, port)
 	logger.Info("server will run on " + addr)
